719fb433fdd0e2fc6d64fa10c169baedb6472bb2
jaeho6627 <jaeho6627@daum.net>
2019-09-23 19:36:42 +0900

    add string copy object

--- C++/stringBilde(정재호_ver1.1).cpp
+++ C++/stringBilde(정재호_ver1.1).cpp
@@ -1,10 +1,16 @@
 /*
-파일명	:	stringBilde(정재호_ver1.0).cpp
+파일명	:	stringBilde(정재호_ver1.1).cpp
 파일목적	:	string 해더파일의 string클래스의 기능을 일부 카피해서 작성
 마지막수정:	2019.09.23
+보완 : 
+1. strlen 과 같은 준비된 함수를 활용
+미 보완 : 
+2. 소멸자 생성해 활용하기
+3. 복사 연산자 활용
 */
 
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -34,42 +40,45 @@ namespace Custom
 
 		string(const char str[])
 		{
-			for (int n_len = 0; str[n_len] != NULL; n_len++)
+			/*for (int n_len = 0; str[n_len] != NULL; n_len++)
 			{
 				len = n_len;
-			}
-			len++;
+			}*/
+			len = strlen(str);		//가능하면 준비된 함수 사용
 
 			m_strString = new char[len+1];	//NULL 문자 저장해야 함 "data"문자열 길이는 4지만 뒤에 NULL까지 포함하면 5
-			for (int i = 0; i <= len; i++)
+			/*for (int i = 0; i <= len; i++)
 			{
 				m_strString[i] = str[i];
-			}
+			}*/
+			memcpy(m_strString, str, len+1);
 		}
 		string(string& str)
 		{
-			len = str.reLen();
+			/*len = str.reLen();*/
+			len = strlen(str.c_str());
 
 			m_strString = new char[len+1];
-			for (int i = 0; i <= len; i++)
+			/*for (int i = 0; i <= len; i++)
 			{
 				m_strString[i] = str.reChar(i);
-			}
+			}*/
+			memcpy(m_strString, str.c_str(), len + 1);
 		}
 		const char* c_str()
 		{
 			return m_strString;
 		}
 		//전달 받은 인텍스 값의 위치에 해당하는 문자 반환
-		char reChar(int idx)
+		/*char reChar(int idx)
 		{
 			return m_strString[idx];
-		}
+		}*/
 		//문자열 길이 반환하는 함수
-		int reLen()
+		/*int reLen()
 		{
 			return len;
-		}
+		}*/
 	};
 
 	void StringMain()
